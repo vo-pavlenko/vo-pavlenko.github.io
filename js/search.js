5420aa64b6ff463ea2f3830308083a86749b89ea
Vsevolod <tankict.92@gmail.com>
2019-10-01 10:14:34 +0300

    проба пера

---
+++ js/search.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+  searchBtn.classList.toggle("close");
+  input.classList.toggle("square");
+};
+console.log(searchBtn);
+searchBtn.addEventListener("click", expand);
