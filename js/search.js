7c9754685e17cffbe3e4c3e1488956344ad36bb2
Vsevolod <tankict.92@gmail.com>
2019-10-07 20:04:43 +0300

    Новый этап

--- js/search.js
+++ js/search.js
@@ -1,9 +1,10 @@
-const input = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
+document.onclick = function(event) {
+    var target = event.target;
+    var s = document.getElementsByClassName('heading__submenu');
+    if (target.className != 'heading__submenu' && s[0].style.display == 'block') {
+        s[0].style.display = 'none';
+    } else if (target.className == 'heading_button' && (s[0].style.display == '' || s[0].style.display == 'none')) {
+        s[0].style.display = 'block';
+    }
 
-const expand = () => {
-  searchBtn.classList.toggle("close");
-  input.classList.toggle("square");
-};
-console.log(searchBtn);
-searchBtn.addEventListener("click", expand);
+}
